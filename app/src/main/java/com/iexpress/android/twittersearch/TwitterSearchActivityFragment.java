ef78b4e8bc39fbdd9f39006652353e399193781d
jwang1 <sigkdd@yahoo.com>
2018-10-10 00:22:48 -0400

    init checkin, blank activity

---
+++ app/src/main/java/com/iexpress/android/twittersearch/TwitterSearchActivityFragment.java
@@ -0,0 +1,22 @@
+package com.iexpress.android.twittersearch;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TwitterSearchActivityFragment extends Fragment {
+
+    public TwitterSearchActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_twitter_search, container, false);
+    }
+}
